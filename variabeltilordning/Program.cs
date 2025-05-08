80603f634c6105ddc2cbd07904a716497c73d725
christis96 <christian.ulstein@gmail.com>
2025-05-08 11:37:58 +0200

    made a function that returns a sum of to inputs, and a function that writes to the console, but doesnt return anything
    Replace Hello World with sum calculation functions
    
    Implemented `returnSum` function to calculate the sum of two integers. Added a variable to store the result of `returnSum(5, 10)` and print it. Introduced `dennemetodenreturnereringenting` method to display a message with the result of `returnSum(6, 9)`.

--- variabeltilordning/Program.cs
+++ variabeltilordning/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+int returnSum(int input1, int input2)
+{
+    return input1 + input2;
+}
+
+int sum = returnSum(5, 10);
+
+Console.WriteLine(sum);
+
+void dennemetodenreturnereringenting()
+{
+    Console.WriteLine($"heisann, dette er en sum av to inputs: {returnSum(6, 9)}");
+}
+dennemetodenreturnereringenting();
